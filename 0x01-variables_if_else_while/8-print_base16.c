cfac8fdf7f7a0781b5090e16f6b4f8eeaaf00f42
RONALDO CHOIGA <choigaronaldo@gmail.com>
2023-03-20 12:23:05 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Return: always 0
+ *
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i);
+	for (i = 'a' ; i <= 'f' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
