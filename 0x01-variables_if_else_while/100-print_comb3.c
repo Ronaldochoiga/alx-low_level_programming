3a25ed8a8f4567d2c3e38391e4bffb92954c20b7
RONALDO CHOIGA <choigaronaldo@gmail.com>
2023-03-21 12:59:00 -0700

    brainstorming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Return: always 0
+ *
+ **/
+int main(void)
+{
+	int i;
+	int j;
+	int ij;
+	int ji;
+
+	for (i = 0 ; i <= 9 ; i++)
+	for (j = 0 ; j <= 9; ij++)
+	{	putchar(ij + '0');
+		if (ij < ji && ((i + j) - (j + i) != 0 ))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
