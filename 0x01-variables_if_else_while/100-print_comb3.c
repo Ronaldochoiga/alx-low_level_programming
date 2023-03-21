58dea903b9ebff3d5d47addcc7eacce8121596c6
RONALDO CHOIGA <choigaronaldo@gmail.com>
2023-03-21 13:10:24 -0700

    brainstorming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
 	for (i = 0 ; i <= 9 ; i++)
 	for (j = 0 ; j <= 9; ij++)
 	{	putchar(ij + '0');
-		if (ij < ji && ((i + j) - (j + i) != 0 ))
+		if (ij < ji && ((i + j) - (j + i) != 0))
 		{
 			putchar(',');
 			putchar(' ');
