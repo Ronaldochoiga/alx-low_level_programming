58441a63992ac3e49552c7f808534302bff8df45
RONALDO CHOIGA <choigaronaldo@gmail.com>
2023-03-22 14:37:02 -0700

    nested 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - ptints 10 times
+ *
+ *Return: always 0
+ *
+ **/
+void print_alphabet_x10(void)
+{
+	int j;
+	char i;
+
+	for (j = 1 ; j <= 10 ; j++)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
