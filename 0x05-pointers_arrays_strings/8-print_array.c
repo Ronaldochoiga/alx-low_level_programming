b3ebefb4bf0d16386a9dbd8d087570b15a4286b6
ronaldochoiga <choigaronaldo@gmail.com>
2023-03-28 16:30:39 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - print array
+ *
+ *@a: function parameter
+ *@n: pointer
+ *
+ *Return: 0
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; a[i] < a[n] ; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
