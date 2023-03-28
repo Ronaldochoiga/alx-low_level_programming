befef43113a349ef49d037aa85e34428127748b2
ronaldochoiga <choigaronaldo@gmail.com>
2023-03-28 14:29:37 -0700

    reversal

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,7 +15,7 @@ void print_rev(char *s)
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for (i = count ; i >= 1 ; i--)
+	for (i = count ; i >= 0 && i != '\0' ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
