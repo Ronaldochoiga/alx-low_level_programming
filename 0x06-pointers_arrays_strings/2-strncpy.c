f77fd3931d00604ec9ca02e856d03dd07cf8b4ed
ronaldochoiga <choigaronaldo@gmail.com>
2023-03-30 14:59:45 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,6 +18,5 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0 ; i < n ; i++)
 		dest[i] = src[i];
-	return (dest)
+	return (dest);
 }
-
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *cap_string - main function
+ *
+ *@s: functional parameter
+ *
+ *Return: always 0
+ *
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j[i + 1];
+
+	char a[] = "32, 9, 10, 44, 59, 46, 33, 63, 40, 41, 123, 125";
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (i == a && 97 >= j <= 122)
+			s[i - 32] = j;
+	}
+	return (s)
+}
