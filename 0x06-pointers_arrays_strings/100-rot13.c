e41d43de73ac88537ce07793b994603debeb3519
ronaldochoiga <choigaronaldo@gmail.com>
2023-03-31 07:28:17 -0700

    trouble in rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,21 +8,24 @@
  *Return: always 0
  *
  **/
-char *rot13(char *s)
+char *rot13(char *r)
 {
 	int i;
 	int j;
 
-	char *a = "ABCDEFGHIJKLM";
-	char *b = "NOPQRSTUVWXYZ";
+	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	for (i = 0 ; r[i] != '\0' ; i++)
 	{
 		for (j = 0 ; a[j] != '\0' ; j++)
 		{
-			if (s[i] == a[j])
-				s[i] = b[j];
+			if (r[i] == a[j])
+			{
+				r[i] = b[j];
+				break;
+			}
 		}
 	}
-	return (s);
+	return (r);
 }
