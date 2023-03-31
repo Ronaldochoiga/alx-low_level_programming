f104262aba25de65fbb6484bb1ea8728bb1540a4
ronaldochoiga <choigaronaldo@gmail.com>
2023-03-31 12:20:21 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,9 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
-	while(s[i])
+	while (s[i])
 	{
-		while(!(s[i] >= 'a' && s[i] <= 'z'))
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
 			i++;
 
 		if (s[i - 1] == ' ' ||
