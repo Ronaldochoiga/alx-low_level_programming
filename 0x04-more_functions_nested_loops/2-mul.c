de54140ee2d7ca14c5049e001cf718f174ad63be
ronaldochoiga <choigaronaldo@gmail.com>
2023-04-01 15:34:31 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - multiplie two integers
+ *
+ *@a: functional parameter integer
+ *
+ *@b: functional parameter integer.*
+ *Return: always 0
+ *
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
