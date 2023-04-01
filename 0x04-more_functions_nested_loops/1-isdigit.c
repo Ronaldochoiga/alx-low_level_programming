21a3e0d9d36275a35a2b0c0aeb2d67bf55bca3ad
ronaldochoiga <choigaronaldo@gmail.com>
2023-04-01 15:24:27 -0700

    digits or not

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_isupper - checks whether upper case or lower case
+ *_isdigit - main function and checks where digit or not
  *
  *@c: functional parameter
  *
@@ -9,9 +9,9 @@
  *
  **/
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if ((c >= 48) && (c <= 57))
 	{
 		return (1);
 	}
