5cc352dc7df4cd209474375d2c84f281bcdf2601
ronaldochoiga <choigaronaldo@gmail.com>
2023-04-01 15:03:12 -0700

    upper or lower cases

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isupper - checks whether upper case or lower case
+ *
+ *@c: functional parameter
+ *
+ *Return: always 0
+ *
+ **/
+
+int _isupper(int c)
+{
+	if ((c == 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
