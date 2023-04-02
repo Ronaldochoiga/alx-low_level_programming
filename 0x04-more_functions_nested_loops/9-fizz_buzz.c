0714b820689878abca9a6ea07bbe813559549877
ronaldochoiga <choigaronaldo@gmail.com>
2023-04-01 18:29:37 -0700

    fizz and buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 	for (i = 1 ; i <= 100 ; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
-			printf("fizz");
+			printf(" fizz");
 		else if (i % 5 == 0 && i % 3 != 0)
-			printf("buzz");
+			printf(" buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("fizzbuzz");
+			printf(" fizzbuzz");
 		else if (i == 1)
 			printf("%d", i);
 		else
