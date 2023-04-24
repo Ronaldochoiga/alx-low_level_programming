1117cd3fa57bda9c22cef40a475f5c7faeddd1cc
ronaldochoiga <choigaronaldo@gmail.com>
2023-04-23 23:26:23 -0700

    first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *@argc: counter
+ *
+ *@argv: vector
+ *
+ *Return: zero
+ *
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+int check_num(char *str);
+int _putchar(char c);
+
+#endif
