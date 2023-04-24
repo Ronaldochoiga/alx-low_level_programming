50fc2c8a70ea5271cc6279c37b46598ca9deadc9
ronaldochoiga <choigaronaldo@gmail.com>
2023-04-24 01:15:45 -0700

    faulty

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - this is the main function
+ *
+ *@argc: the is the functional parameter(counter)
+ *
+ *@argv: this is the parameter vector
+ *
+ *Return: zero
+ *
+ **/
+
+int main(int argc, char *argv[])
+
+{
+	int i = 0;
+
+	if (argc >= 0)
+	{
+		for (i = 0 ; i <= argc ; i++)
+		{
+			printf("%s\n", argv[i]);
+			if (i == argc)
+			{
+				i++;
+				break;
+			}
+		}
+	}
+	return (i);
+}
