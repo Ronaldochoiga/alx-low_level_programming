c13e1323b0abde63a266d884765ae60829ceab15
ronaldochoiga <choigaronaldo@gmail.com>
2023-04-30 09:20:50 -0700

    task 6

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,15 +11,15 @@
 
 int factorial(int n)
 {
-	int r = n;
+	int i = n;
 
 	if (n < 0)
 		return (-1);
 	else if (n >= 0 && n <= 1)
 		return (-1);
 	{
-		r *= factorial(n - 1);
+		i *= factorial(n - 1);
 
-		return (r);
+		return (i);
 	}
 }
