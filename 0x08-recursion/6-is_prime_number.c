4d21cffb0530089a458075e5dec20d48c5b7e9ee
ronaldochoiga <choigaronaldo@gmail.com>
2023-04-30 09:17:00 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 int is_prime_number(int n);
-int is_divisible(int x, int d);
+int is_divisible(int num, int div);
 /**
  *is_divisible - main parameter
  *
  *is_prime_number - main parameter
  *
- *@x: functional parameter that is the number to be evaluated
+ *@num: functional parameter that is the number to be evaluated
  *
- *@d: functional parameter and dividing number
+ *@div: functional parameter and dividing number
  *
  *Return: 0 or 1
  *
  **/
-int is_divisible(int x, int d)
+int is_divisible(int num, int div)
 {
-	if (x % d == 0)
+	if (num % div == 0)
 		return (0);
-	if (d == x / 2)
+	if (div == num / 2)
 		return (1);
-	return (is_divisible(x, d + 1));
+	return (is_divisible(num, div + 1));
 }
