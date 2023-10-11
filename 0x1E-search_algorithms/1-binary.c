15f460e047e0f4b49216a5ef79dd84b0535a658e
ronaldochoiga <choigaronaldo@gmail.com>
2023-10-11 12:07:42 -0700

    task 2

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,65 @@
 #include "search_algos.h"
-void atos(int *array, size_t from, size_t to);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int binary_help(int *array, int value, size_t low, size_t high);
+void print_array(int *array, size_t low, size_t high);
 /**
- * Binary_search - this invilves a binary tied algorithm
- *@array: this is the pointer to the first elemnt of an array
- *@size: this is the size of an array to be searched in.
- *@value: this is the value to be searched in the array.
- * Return: returns the location of the value in index for or
- * -1 for a null such where it cannot be located.
- **/
+ * binary_search - function that searcges for a value in a sorted array
+ * @array: pointer to element 1
+ * @size: no of elems inj the array to search in
+ * @value: the actual value to be searched for
+ * Return: index of the value found or -1 if the value is not found hence null
+ */
 int binary_search(int *array, size_t size, int value)
 {
-  size_t i = 0, j = size = -1;
-
-  if (array)
-    while (i <= j)
-      {
-	size_t m = (j - i) / 2 + i;
-
-	atos(array, i, j);
-	if (array[m] == value)
-	  return m;
-	if (value > array[m])
-	  i = m + 1;
-	else
-	  j = m - 1;
-      }
-  return (-1);
+	if (!array || size == 0)
+		return (-1);
+	return (binary_help(array, value, 0, size - 1));
 }
-/**
- *atos - to print an array or a sub_array.
- *@array: this is the array to be printed
- *@from: this is the start point of the array to be printed
- *@to: this is the end point of the array.
- *Return: this is the array to be printed.
- **/
-void atos(int *array, size_t from, size_t to)
-{
-  size_t i;
 
-  printf("searching in array: ");
-  for (i = from; i < to; i++)
-    printf("%d, ", array[i]);
-  printf("%d\n", array[i]);
+/**
+ * binary_help - search for value in an array in sorted format
+ * @array: pointer rto element 1
+ * @value: actual value to be searched for
+ * @low: lesser index value
+ * @high: upper index value
+ * Return: index of the value location or -1 for the null or unavaillable
+ */
+int binary_help(int *array, int value, size_t low, size_t high)
+{
+	size_t m;
+
+	print_array(array, low, high);
+	if (high == low && array[low] != value)
+		return (-1);
+	m = ((high - low) / 2) + low;
+	if (array[m] == value)
+		return (m);
+	if (array[m] < value)
+		return (binary_help(array, value, m + 1, high));
+	if (array[m] > value)
+		return (binary_help(array, value, low, m - 1));
+	return (-1);
+}
+
+/**
+ * print_array - prints an array passed as parameter
+ * @array: Pointer to the first elem
+ * @low: lesser index value
+ * @high: upper index value
+ */
+void print_array(int *array, size_t low, size_t high)
+{
+	size_t i = low;
+
+	printf("Searching in array: ");
+	while (i <= high)
+	{
+		printf("%d", array[i]);
+		if (i < high)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
 }
