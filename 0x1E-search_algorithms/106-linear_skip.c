1fc2dfc60f27fbb99080a6aa4240354be5686dcc
ronaldochoiga <choigaronaldo@gmail.com>
2023-10-11 14:24:47 -0700

    task 106 correction

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -2,16 +2,12 @@
 
 /**
  * linear_skip - Searches for an algorithm in a sorted singly
- *               linked list of integers using linear skip.
+ *               linked list
  * @list: A pointer to the  head of the linked list to search.
  * @value: The value to search for.
- *
  * Return: If the value is not present or the head of the list is NULL, NULL.
- *         Otherwise, a pointer to the first node where the value is located.
- *
- * Description: Prints a value every time it is compared in the list.
- *              Uses the square root of the list size as the jump step.
- */
+ *         Otherwise, a pointer to the first node where the value is located
+ **/
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *node, *jump;
